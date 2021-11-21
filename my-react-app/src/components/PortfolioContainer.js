cda46a0710225d0bc266da728094ba415e98274b
Matt Weichel <maweiche@gmail.com>
2021-11-21 14:56:05 -0800

    added download resume and image/resume files

--- my-react-app/src/components/PortfolioContainer.js
+++ my-react-app/src/components/PortfolioContainer.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home'
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
 
 
     const renderPage = () => {
-        if (currentPage === 'Home') {
-            return <Home />;
-        }
         if (currentPage === 'About') {
             return <About />;
         }
         if (currentPage === 'Projects') {
             return <Projects />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
         }
             return <Contact />;
     };
---
+++ my-react-app/src/components/pages/Resume.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Resume() {
+    return (
+        <div>
+            <a id="resume" href="./assets/files/MW2021.pdf" target="_blank" download="MWresume"><h5>Download My Resume</h5></a>
+        </div>
+    );
+}
